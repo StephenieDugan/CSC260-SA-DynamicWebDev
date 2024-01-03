a8a47469124cf44194d82564f9e4e1aaa8a3a096
StephenieDugan <stepheniedugan617@gmail.com>
2024-01-03 12:59:56 -0700

    Add project files.

---
+++ ClassProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClassProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
