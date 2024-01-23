443d7394cf194e44c629783b999faa84bb6c36fe
StephenieDugan <stepheniedugan617@gmail.com>
2024-01-23 12:02:30 -0700

    formatting, editing, and deleting

--- AboutMeProject/Controllers/VideoGameController.cs
+++ AboutMeProject/Controllers/VideoGameController.cs
@@ -12,6 +12,51 @@ namespace AboutMeProject.Controllers
             new VideoGame("Spider-Man Across the Spider-Verse", 2023, 5f,"https://tse2.mm.bing.net/th?id=OIP.BUuP7rMyLxN0pXmhNQxapAHaHa&pid=Api&P=0&h=220"),
             new VideoGame("How to Train Your Dragon: Lost World", 2010, 5f,"https://tse2.mm.bing.net/th?id=OIP.BUuP7rMyLxN0pXmhNQxapAHaHa&pid=Api&P=0&h=220")
         };
+        [HttpGet]//loads edit page
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+               return NotFound();
+            }
+            else
+            {
+                int i = gameList.FindIndex(g => g.Id==id);
+                if(id ==-1)
+                   return NotFound();
+                gameList.RemoveAt(i);
+                TempData["Success"] = "Game Deleted";
+                return RedirectToAction("MultGames", "VideoGame");
+            }
+        }
+        [HttpGet]//loads edit page
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                ViewData["Error"] = "Game data not found";
+                return View();
+            }
+            else
+            {
+                VideoGame? g = gameList.Where(g => g.Id == id).FirstOrDefault();
+                if(g == null)
+                {
+                    ViewData["Error"] = "Game not found"; // cant find game with this id
+                }
+                return View(g);
+            }
+        }
+        [HttpPost]//pushes edits
+        public IActionResult Edit(VideoGame g)
+        {
+            int i;
+            i = gameList.FindIndex(x =>x.Id == g.Id);
+            gameList[i] = g;
+            TempData["Success"] = "Game Updated";
+            return RedirectToAction("MultGames", "VideoGame");
+        }
+
 
         [HttpGet]
         public IActionResult Add()
